a42ba320b4e5c81d9abdd45c998c354393952616
Curtes Malteser <antonio_bastiao@hotmail.com>
2021-02-21 17:07:26 +0100

    added white bottom collection view and the correspondent animation

--- AnimFy/ViewController/SettingsLaucher.swift
+++ AnimFy/ViewController/SettingsLaucher.swift
@@ -12,32 +12,48 @@ class SettingsLauncher: NSObject {
 
     private let blackView = UIView()
 
+    private var _cv: UICollectionView!
+
+    private var collectionView: (_ frameWidth: CGFloat, _ frameHeight: CGFloat) -> UICollectionView = { w, h in
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.frame = CGRect(x: 0, y: h, width: w, height: 200)
+        cv.backgroundColor = .white
+        return cv
+    }
+
     override init() {
         super.init()
     }
 
 
     func showSettings() {
-        print("more anime")
 
         if let window = UIApplication.firstKeyWindow() {
             blackView.backgroundColor = UIColor(white: 0, alpha: 0.5)
-
             blackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleDismiss)))
-
-            window.addSubview(blackView)
             blackView.frame = window.frame
             blackView.alpha = 0
 
-            UIView.animate(withDuration: 0.6) {
+            window.addSubview(blackView)
+
+            _cv = collectionView(window.frame.width, window.frame.height)
+            window.addSubview(_cv)
+
+            UIView.animate(withDuration: 0.4) {
                 self.blackView.alpha = 1
+
+                let y = window.frame.height - self._cv.frame.height
+                self._cv.frame = CGRect(x: 0, y: y, width: self._cv.frame.width, height: self._cv.frame.height)
             }
         }
+
     }
 
     @objc private func handleDismiss() {
         UIView.animate(withDuration: 0.4) {
             self.blackView.alpha = 0
+            self._cv.frame = CGRect(x: 0, y: self.blackView.frame.height, width: self._cv.frame.width, height: self._cv.frame.height)
         }
     }
 
