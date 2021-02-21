b344aee442b336a090a2cc38ed863b10c58f0511
Curtes Malteser <antonio_bastiao@hotmail.com>
2021-02-21 15:56:04 +0100

    implemented black background dismiss

--- AnimFy/ViewController/AnimeCollectionVC.swift
+++ AnimFy/ViewController/AnimeCollectionVC.swift
@@ -30,23 +30,32 @@ class AnimeCollectionVC: BaseCollectionVC, BaseCollectionDelegate {
         dataRepository?.statusDelegate = statusDelegate
     }
 
+    let blackView = UIView()
+
     /// source: https://www.youtube.com/watch?v=2kwCfFG5fDA
     @objc private func setupAnimeMenu() {
         print("more anime")
 
         if let window = UIApplication.firstKeyWindow() {
-            let blackView = UIView()
             blackView.backgroundColor = UIColor(white: 0, alpha: 0.5)
+
+            blackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleDismiss)))
 
             window.addSubview(blackView)
             blackView.frame = window.frame
             blackView.alpha = 0
 
             UIView.animate(withDuration: 0.6) {
-                blackView.alpha = 1
+                self.blackView.alpha = 1
             }
         }
 
+    }
+
+    @objc private func handleDismiss() {
+        UIView.animate(withDuration: 0.4) {
+            self.blackView.alpha = 0
+        }
     }
 
     override func viewDidAppear(_ animated: Bool) {
